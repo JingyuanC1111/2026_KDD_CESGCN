c1a03ff890ccd2e53f02da9d8bf11c389884af79
JingyuanC1111 <143842150+JingyuanC1111@users.noreply.github.com>
2025-12-17 22:01:10 -0500

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('CDC_DATA/target-hospital-admissions_20251206.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/target-data/target-hospital-admissions.csv')
 removable = ['US']
 df['date'] = pd.to_datetime(df['date']).dt.date
 df = df[~df['location'].isin(removable)]
@@ -10,4 +10,4 @@ available_data = df.pivot(index='location', columns='date', values='value')
 available_data = available_data.T
 available_data.fillna(0, inplace=True)
 available_data = available_data.iloc[1:,:]
-available_data.to_csv('CDC_DATA/transferred_hospital_admission_20251206.csv')
+available_data.to_csv('CDC_DATA/transferred_hospital_admission_latest.csv')
